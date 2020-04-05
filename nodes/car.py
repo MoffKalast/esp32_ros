c0390e430580c23cf957cdd03cecf2302ff66e95
MoffKalast <vid.rijavec@gmail.com>
2020-04-05 13:39:00 -0400

    camera

--- nodes/car.py
+++ nodes/car.py
@@ -19,7 +19,7 @@ class ESP32:
 		self.run = True
 
 		self.ws = websocket.WebSocket()
-		self.ws.connect("ws://192.168.1.8")
+		self.ws.connect("ws://192.168.1.8:8080")
 
 		print("Connected.")
 
@@ -69,7 +69,7 @@ class ESP32:
 			while not connected:
 				try:
 					self.ws = websocket.WebSocket()
-					self.ws.connect("ws://192.168.1.8")
+					self.ws.connect("ws://192.168.1.8:8080")
 					connected = True
 				except:
 					print(sys.exc_info()[0])
